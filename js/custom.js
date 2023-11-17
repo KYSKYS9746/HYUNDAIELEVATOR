f87a4e89bf8ced14c0b6eee918e859d28e9fc97f
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2023-11-17 10:52:41 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -74,4 +74,50 @@ $('.MainVisual .play').on('click', function() {
         
 
     })
+
+    $('.MainSolution .solution_slide').on('afterChange', function(e,s,c){
+        //c = 0,1,2....8
+        $('.MainSolution .solusion_txt .txt_box li').removeClass('on');
+        $('.MainSolution .solusion_txt .txt_box li').eq(c).addClass('on');
+        // 숫자 넘기기
+        $('.MainSolution .solution_idx strong').text('0' + (c + 1));
+
+    });
+
+    $('#promotion').YTPlayer({
+        videoURL:'https://youtu.be/5nnsleIF6Yw',
+        containment:'.mainPromotion .movie_case',
+        showControls: false,
+        autoPlay:false,
+        optimizeDisplay: false,
+        playOnlyIfVisible: true,
+        
+        
+
+        // autoPlay:true, 
+        // mute:true,
+        //  startAt:0, 
+        //  opacity:1,
+    });
+
+    let movie = true;
+
+
+    $('.mainPromotion .movie_play').on('click', function(){
+        if(movie) {
+            $('#promotion').YTPPlay();
+        }else {
+            $('#promotion').YTPPause();
+        }
+
+        $(this).toggleClass('on');
+
+
+        movie = !movie;
+
+       
+    });
+
+
+
 });
